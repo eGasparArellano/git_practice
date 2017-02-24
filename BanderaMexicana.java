2df4901992f5f1176ca8944002564aeeca58f471
eGasparArellano <eGasparArellano@gmail.com>
2017-02-24 08:18:07 -0600

    First Commit en Git

---
+++ BanderaMexicana.java
@@ -0,0 +1,10 @@
+public class BanderaMexicana{
+	private Color verde;
+	private Color blanco;
+	private Color rojo;
+	private Img escudo;
+	
+	public BanderaMexicana(){
+		//Mi primera practica en Git
+	}	
+}
